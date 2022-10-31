61be22aa770f9d914321bb675b2615b8d863718a
HongGunWoo <rjsdn9809@naver.com>
2022-10-31 14:14:38 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# 장고 함수인 path 와 blog 애플리케이션에서 사용할 모든 views를 import함(가져옴)
+from . import views # .은 현재 내가있는 위치를 말함
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+def post_list(request):
+    from django.shortcuts import render
+    return render(request, 'blog/post_list.html', {})
 
-# Create your views here.
